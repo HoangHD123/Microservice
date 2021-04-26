a396804bf080c2948c0eb79c3e2f8dfcc6b14e5c
luuhuyhoang <luuhuyhoang@f88.vn>
2021-04-26 16:26:07 +0700

    ok

---
+++ Sample/Middleware/WebUIMiddleware/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebUIMiddleware.Models;
+
+namespace WebUIMiddleware.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        [Route("/MyMiddleWareAddHeader")]
+        public object MyMiddleWareAddHeader(string param)
+        {
+            return JsonConvert.SerializeObject(HttpContext.Request.QueryString.Add(JsonConvert.SerializeObject(HttpContext.Request.Headers), param));
+        }
+    }
+}
---
+++ Sample/Middleware/WebUIMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUIMiddleware
+{
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public MyCustomMiddleware(RequestDelegate next, ILoggerFactory logFactory)
+        {
+            _next = next;
+
+            _logger = logFactory.CreateLogger("MyCustomMiddleware");
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            
+            foreach (var item in httpContext.Request.Query)
+            {
+                var data = item;
+                httpContext.Request.Headers[item.Key] = item.Value;
+            }
+            _logger.LogInformation("My Custom Middleware is executing..");
+
+            await _next(httpContext); // calling next middleware
+        }
+    }
+
+}
